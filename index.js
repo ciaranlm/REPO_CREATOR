7165b532be5711920bcf7463bd887f8fd33bfafe
Ciaran Mulholland <ciaran.l.mul@googlemail.com>
2016-06-27 15:53:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'GitHubRepoHome.html' });
-});
-
-module.exports = router;
+web: node web.js
